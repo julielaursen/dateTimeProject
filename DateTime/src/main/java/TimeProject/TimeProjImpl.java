da6571b8a13e0a9c6c1d27bb1af24eb472f35243
Julie Laursen <julielaursen1@gmail.com>
2020-10-11 19:18:18 -0500

    Add files via upload

--- DateTime/src/main/java/TimeProject/TimeProjImpl.java
+++ DateTime/src/main/java/TimeProject/TimeProjImpl.java
@@ -2,60 +2,115 @@ package TimeProject;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import static org.junit.Assert.*;
 
 public class TimeProjImpl implements TimeProject {
 
     public String timeFormat = "12:00 AM";
 
-    Pattern pattern = Pattern.compile("\\d{2}:\\d{2} [a-zA-Z][a-zA-Z]");
+    Pattern pattern = Pattern.compile("([0-12]|[1-9]):([0-5][0-9])\\s[A-Z][A-Z]");
     Matcher timeMatcher = pattern.matcher(timeFormat);
 
     private String displayTime;
+    private String currentAmPm;
+    private int currentHours;
+    private int currentMin;
+    private int current24hour;
+    private int new24hour;
+    private int minutesAfterAdd;
     private String newTime;
-    private int numMin;
+    private String strCurrentMin;
 
     public TimeProjImpl() {
     }
 
     public void validateTime(String displayTime) {
-        assert timeMatcher.matches() : "Time entered is invalid, please enter the time in the format of 'HH:MM AM|PM'";
+        assert !timeMatcher.matches() : "Time entered is invalid, please enter the time in the format of 'HH:MM AM|PM'";
     }
 
-    //@Override not working
-    public String addMinToTime(String time, int minutes) {
-        validateTime(time);
+    public void parserToHourMin(String displayTime){
+        currentMin = Integer.parseInt(displayTime.substring(3, 5));
+        currentHours = Integer.parseInt(displayTime.substring(0, 2));
+        currentAmPm = displayTime.substring(6, 8);
+    }
 
-        //here int changes 00 to 0, need to use float or something else
-        int minEntered = Integer.parseInt(time.substring(3, 5));
-        int hourEntered = Integer.parseInt(time.substring(0, 2));
-        int newMinInTime = 0;
-        int newHourInTime = 0;
-        String amOrPm = time.substring(5, 6);
-        String newTime = "";
-        int minCheck = minEntered + minutes;
-        //System.out.println("Minutes left in the hour + minutes added is " + minCheck);
-
-        if (minutes > 60 || minCheck > 60) {
-            int addedHours = minutes / 60;
-            int remainingMin = minutes % 60;
-
-            //newMinInTime = remainingMin;
-            //newHourInTime = hourEntered + addedHours;
-            String strMinInTime = String.valueOf(newMinInTime);
-            String strHourInTime = String.valueOf(newHourInTime);
-            newTime = strHourInTime + ":" + strMinInTime + " " + amOrPm;
-        } else {
-            newMinInTime = minEntered + minutes;
-            String strMinInTime = String.valueOf(newMinInTime);
-            String strHourInTime = String.valueOf(hourEntered);
-            //System.out.println(hourInTime);
-            newTime = strHourInTime + ":" + strMinInTime + " " + amOrPm;
+    public void conversionTo24(int currentMin, int currentHours, String currentAmPm){
+        if (currentHours == 12 && currentAmPm.equals("AM")){
+            this.currentHours = 0;
         }
-        validateTime(newTime);
-        System.out.println("Time entered :" + time);
-        System.out.println("Minutes added :" + minutes);
-        System.out.println("The new time is " + newTime);
+        else if (currentHours != 12 && currentAmPm.equals("PM")){
+            this.currentHours = currentHours + 12;
+        }
+    }
 
+    public void conversionToMin(int currentHours, int currentMin){
+        this.new24hour = ((currentHours * 60) + currentMin);
+    }
+
+    public void newConversionTo24(int minutesAfterAdd){
+        this.currentHours = minutesAfterAdd / 60;
+            if(this.currentHours > 23){
+                this.currentHours = this.currentHours - 24;
+            }
+        this.currentMin = minutesAfterAdd % 60;
+    }
+
+    public void convert24toAmPm(int hours, int min, int minutes){
+        if(hours == 0){
+            this.currentHours = 12;
+            this.currentMin = min;
+            this.currentAmPm = "AM";
+        }
+        else if(hours == 12){
+            this.currentHours = hours;
+            this.currentMin = min;
+            this.currentAmPm = "PM";
+        }
+        else if(hours > 12){
+            this.currentHours = hours - 12;
+            this.currentMin = min;
+            this.currentAmPm = "PM";
+        }
+        else if(hours < 12){
+            this.currentHours = hours;
+            this.currentMin = min;
+            this.currentAmPm = "AM";
+        }
+        //need another conversion logic to change Pm to AM if hours go into the next day
+
+    }
+
+    public boolean validateMinUnder10(int currentMin) {
+            if (currentMin < 10) {
+                return true;
+            } else {
+                return false;
+            }
+    }
+
+
+    public String addMinToTime(String displayTime, int minutes) {
+        validateTime(displayTime);
+        parserToHourMin(displayTime);
+        conversionTo24(currentMin, currentHours, currentAmPm);
+        conversionToMin(this.currentHours, this.currentMin);
+
+        minutesAfterAdd = this.new24hour + minutes;
+
+        newConversionTo24(minutesAfterAdd);
+        convert24toAmPm(this.currentHours, this.currentMin, minutes);
+
+        System.out.println("Time entered :" + displayTime);
+        System.out.println("Minutes added :" + minutes);
+
+        if(validateMinUnder10(currentMin)){
+            newTime = (currentHours + ":" + String.format("%02d", currentMin) + " " + currentAmPm);
+        }
+        else{
+            newTime = (currentHours + ":" + currentMin + " " + currentAmPm);
+        }
+
+        validateTime(newTime);
         return newTime;
     }
 
--- DateTime/src/test/java/TimeProjectTests.java
+++ DateTime/src/test/java/TimeProjectTests.java
@@ -2,44 +2,152 @@ import org.junit.Test;
 import TimeProject.TimeProjImpl;
 import TimeProject.TimeProject;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class TimeProjectTests {
 
     @Test
-    public void testString1() {
-        TimeProject test1 = new TimeProjImpl();
-        test1.addMinToTime("12:33 PM", 1);
-     }
-
-    @Test
-    public void testString2() {
-        TimeProject test2 = new TimeProjImpl();
-        test2.addMinToTime("12:33 PM", 0);
+    public void addOneMin() {
+        try {
+            TimeProject addOneMin = new TimeProjImpl();
+            String returnValue = addOneMin.addMinToTime("12:33 PM", 1);
+            System.out.println("New value: " + returnValue);
+            assert returnValue.equals("12:34 PM");
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
-    public void testString3() {
-        TimeProject test3 = new TimeProjImpl();
-        test3.addMinToTime("12:33 PM", 30);
+    public void addZeroMin() {
+        try {
+            TimeProject addZeroMin = new TimeProjImpl();
+            String returnValue = addZeroMin.addMinToTime("12:33 PM", 0);
+            System.out.println("New value: " + returnValue);
+            assert returnValue.equals("12:33 PM");
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
-    public void testString4() {
-        TimeProject test3 = new TimeProjImpl();
-        test3.addMinToTime("12:33 PM", 60);
+    public void returnThreePastHour() {
+        try {
+            TimeProject returnThreePastHour = new TimeProjImpl();
+            String returnValue = returnThreePastHour.addMinToTime("12:33 PM", 30);
+            System.out.println("New value: " + returnValue);
+            assert returnValue.equals("1:03 PM");
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
-    public void testString5() {
-        TimeProject test3 = new TimeProjImpl();
-        test3.addMinToTime("12:33 PM", 61);
+    public void addOneHour() {
+        try {
+            TimeProject addOneHour = new TimeProjImpl();
+            String returnValue = addOneHour.addMinToTime("12:33 PM", 60);
+            System.out.println("New value: " + returnValue);
+            assert returnValue.equals("1:33 PM");
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
-    public void testString6() {
-        TimeProject test3 = new TimeProjImpl();
-        test3.addMinToTime("12:33 PM", 1000000);
+    public void addOneHourOneMin() {
+        try {
+            TimeProject addOneHourOneMin = new TimeProjImpl();
+            String returnValue = addOneHourOneMin.addMinToTime("12:33 PM", 61);
+            System.out.println("New value " + returnValue);
+            assert returnValue.equals("1:34 PM");
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
     }
 
+    @Test
+    public void testMidnightStr() {
+        try {
+            TimeProject testMidnightStr = new TimeProjImpl();
+            String returnValue = testMidnightStr.addMinToTime("12:00 AM", 0);
+            System.out.println("New value " + returnValue);
+            assert returnValue.equals("12:00 AM");
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testLargeNum() {
+        try {
+            TimeProject testLargeNum = new TimeProjImpl();
+            String returnValue = testLargeNum.addMinToTime("12:00 AM", 1439);
+            System.out.println("New value " + returnValue);
+            assert returnValue.equals("11:59 PM");
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testMaxBoundaryValue() {
+        try {
+            TimeProject testMaxBoundaryValue = new TimeProjImpl();
+            String returnValue = testMaxBoundaryValue.addMinToTime("12:00 AM", 1440);
+            System.out.println("New value " + returnValue);
+            assert returnValue.equals("12:00 AM");
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testMaxPlusOneBoundaryValue1() {
+        try {
+            TimeProject testMaxPlusOneBoundaryValue1 = new TimeProjImpl();
+            String returnValue = testMaxPlusOneBoundaryValue1.addMinToTime("12:00 AM", 1441);
+            System.out.println("New value " + returnValue);
+            assert returnValue.equals("12:01 AM");
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testBoundaryValue2() {
+        try {
+            TimeProject testBoundaryValue2 = new TimeProjImpl();
+            String returnValue = testBoundaryValue2.addMinToTime("11:00 PM", 120);
+            System.out.println("New value " + returnValue);
+            assert returnValue.equals("1:00 AM");
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void malformedRequest() {
+        TimeProject malformedRequest = new TimeProjImpl();
+        try {
+            String returnValue = malformedRequest.addMinToTime("1100PM", 120);
+            fail("Should have thrown an exception");
+        } catch (final RuntimeException e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void malformedRequest2() {
+        TimeProject malformedRequest2 = new TimeProjImpl();
+        try {
+            String returnValue = malformedRequest2.addMinToTime("#R@#$*&", 120);
+            fail("Should have thrown an exception");
+        } catch (final RuntimeException e) {
+            assertTrue(true);
+        }
+    }
 }
 
 
